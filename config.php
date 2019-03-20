b837354f6bd1064cbfa54463b9a01fea9cf2b0fb
kyrre <kyrre@oslomet.no>
2019-03-20 09:27:35 +0100

    More changes to config.php file

--- config.php
+++ config.php
@@ -6,4 +6,13 @@ $dbuser = "bfuser";
 $dbpassw = '';
 $webhost = '';
 $weburl = 'http://' . $webhost ;
+$frontpage_limit = 100000;
+if ( getenv("BF_FRONTPAGE_LIMIT") ){
+   $frontpage_limit = getenv("BF_FRONTPAGE_LIMIT");
+}
+if ( getenv("BF_MEMCACHE_SERVER")){
+   $memcache_enabled_pictures = 1;
+   $memcache_server = getenv("BF_MEMCACHE_SERVER");
+   $memcache_enabled = 1;
+}
 ?>
