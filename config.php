71400fe9c55e221f838c568562fddd0f4b5705dc
Kyrre <kyrre@begnum.no>
2023-01-05 11:59:47 +0100

    adjusted frontpage limit and moved config.php files in order to be more intuitive

--- config.php
+++ config.php
@@ -1,20 +1,10 @@
 <?php
-$use_local_images = 0;
-$dbhost = getenv("BF_DB_HOST");
-$dbport =  getenv("BF_DB_PORT");
-$db = getenv("BF_DB_NAME");
-$dbuser = getenv("BF_DB_USER");
-$dbpassw = getenv("BF_DB_PASS");
-$webhost = getenv("BF_WEBHOST");
-$imagepath = getenv("BF_IMAGE_PATH");
+$dbhost = "";
+$dbport = "3306";
+$db = "";
+$dbuser = "";
+$dbpassw = '';
+$webhost = '';
 $weburl = 'http://' . $webhost ;
-$frontpage_limit = 100000;
-if ( getenv("BF_FRONTPAGE_LIMIT") ){
-   $frontpage_limit = getenv("BF_FRONTPAGE_LIMIT");
-}
-if ( getenv("BF_MEMCACHE_SERVER")){
-   $memcache_enabled_pictures = 1;
-   $memcache_server = getenv("BF_MEMCACHE_SERVER");
-   $memcache_enabled = 1;
-}
+$frontpage_limit = "1000";
 ?>
--- config_example.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbhost = "";
-$dbport = "3306";
-$db = "";
-$dbuser = "";
-$dbpassw = '';
-$webhost = '';
-$weburl = 'http://' . $webhost ;
-?>
